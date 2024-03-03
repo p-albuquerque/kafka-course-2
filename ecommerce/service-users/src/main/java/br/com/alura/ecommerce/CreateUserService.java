9af1de26feac5b1e19b72fe89d2debdd4cb9fccf
Paulo Albuquerque <paulo.albuquerque@liferay.com>
2024-03-03 10:24:00 -0300

    C-02 Create service-user module. Add dependencies: sqlLite (will handle the users persistence) and kafka commons. Create main class preset and bring an Order copy into the module.

---
+++ ecommerce/service-users/src/main/java/br/com/alura/ecommerce/CreateUserService.java
@@ -0,0 +1,27 @@
+package br.com.alura.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+public class CreateUserService {
+    public static void main(String[] args) {
+        var createUserService = new CreateUserService();
+        try (var service = new KafkaService<>(CreateUserService.class.getSimpleName(),
+                "ECOMMERCE_NEW_ORDER",
+                createUserService::parse,
+                Order.class,
+                Map.of())) {
+            service.run();
+        }
+    }
+
+    private void parse(ConsumerRecord<String, Order> record) throws ExecutionException, InterruptedException {
+        System.out.println("------------------------------------------");
+        System.out.println("Processing new order, checking for new user");
+        System.out.println(record.value());
+        // acessar o banco para salvar, caso seja novo usuario
+    }
+
+}
